da358721ffa9cfe01501b62809c0b9e47c9b7818
Burak Olan <67985010+reburak@users.noreply.github.com>
2025-08-20 15:31:29 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import shutil
 CRD_SSH_Code = input("Google CRD SSH Code :")
 username = "user" #@param {type:"string"}
 password = "root" #@param {type:"string"}
-os.system(f"useradd -m {username}")
-os.system(f"adduser {username} sudo")
+os.system(f'useradd -m {username}')
+os.system(f'adduser {username} sudo')
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
 os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
 
